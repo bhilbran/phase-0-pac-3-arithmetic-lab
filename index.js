d519dc650965d2e8e93557799cbe7c284eca5206
brooke <bhilbran@asu.edu>
2023-06-14 14:40:05 -0700

    done

---
+++ index.js
@@ -0,0 +1,34 @@
+function add() {
+   
+  }
+function subtract() {
+
+}
+function add(x, y) {    
+    return x + y;
+    console.log(add(1,2));
+}
+function subtract(x, y) {    
+    return x - y;
+    console.log(add(1,2));
+}
+function multiply(x, y) {    
+    return x * y;
+    console.log(add(1,2));
+}
+function divide(x, y) {    
+    return x / y;
+    console.log(add(1,2));
+}
+function increment(n){
+    return n=n+1
+}
+function decrement(n){
+    return n=n-1
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
